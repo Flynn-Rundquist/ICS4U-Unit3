9c41e18945f6916a55a10ffbdd9e999f72967afb
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-05-09 09:12:28 -0400

    ts working. added isRepeat

--- Unit3-05/index.ts
+++ Unit3-05/index.ts
@@ -6,29 +6,28 @@
  * Since: 2024-05-07
  */
 
-// find all magic squares
-function findMagicSquares(square: number[], index: number) {
-    for (let counter = 0; counter < 9; counter++) {
-        numProcess++
-        square[index] = counter + 1
+// generate magic squares
+function genSquare(square: number[], index: number) {
+    let numberOfSquares = 0
+    let proccessNum = 0
 
+    for (let counter = 0; counter < 9; counter++) {
+        square[index] = counter + 1
+        proccessNum++
         if (index < 8) {
-            findMagicSquares(square, index + 1)
+            genSquare(square, index + 1)
         } else {
-            if (isMagicSquare(square)) {
-                numOfSquares++
+            if (isMagic(square) && isRepeat(square)) {
                 printSquare(square)
+                numberOfSquares++
             }
         }
     }
 }
 
-// set magicNumber sum
-let numProcess = 0
-let numOfSquares = 0
-
-// check if array is a magic square
-function isMagicSquare(preSquare: number[]) {
+// check if a square is magic
+function isMagic(preSquare: number[]) {
+    const magicSum = 15
     const row1 = preSquare[0] + preSquare[1] + preSquare[2]
     const row2 = preSquare[3] + preSquare[4] + preSquare[5]
     const row3 = preSquare[6] + preSquare[7] + preSquare[8]
@@ -37,34 +36,32 @@ function isMagicSquare(preSquare: number[]) {
     const col3 = preSquare[2] + preSquare[5] + preSquare[8]
     const diag1 = preSquare[0] + preSquare[4] + preSquare[8]
     const diag2 = preSquare[2] + preSquare[4] + preSquare[6]
-    const magicSum = 15
-    return (
-        row1 == magicSum &&
-        row2 == magicSum &&
-        row3 == magicSum &&
-        col1 == magicSum &&
-        col2 == magicSum &&
-        col3 == magicSum &&
-        diag1 == magicSum &&
-        diag2 == magicSum
-    )
+    
+    return row1 === magicSum && row2 === magicSum && row3 === magicSum && col1 === magicSum && col2 === magicSum && col3 === magicSum && diag1 === magicSum && diag2 === magicSum
+}
+
+// checks if square repeats (true if it doesn't, false if it does)
+function isRepeat(square: number[]) {
+    for (let counter = 0; counter < 9; counter++) {
+        for (let counter2 = counter + 1; counter2 < 9; counter2++) {
+            if (square[counter] === square[counter2]) {
+                return false
+            }
+        }
+    }
+    return true
 }
 
 // print square
-function printSquare(outputSquare: number[]) {
-    console.log("\n*****")
-
-    for (let count = 0; count < outputSquare.length; count++) {
-        if (count % 3 == 0 && count != 0) {
-            console.log()
-        }
-        console.log(outputSquare[count])
-    }
-    console.log("\n*****")
+function printSquare(square: number[]) {
+    console.log('\n*****')
+    console.log(square[0] + ' ' + square[1] + ' ' + square[2])
+    console.log(square[3] + ' ' + square[4] + ' ' + square[5])
+    console.log(square[6] + ' ' + square[7] + ' ' + square[8])
+    console.log('*****\n')
 }
 
-// initialise array
-const square: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-console.log("Magic Squares\n")
-findMagicSquares(square, 0)
-console.log("\nDone")
+// main
+const magicSquare = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+console.log("The magic squares are: ")
+genSquare(magicSquare, 0)
