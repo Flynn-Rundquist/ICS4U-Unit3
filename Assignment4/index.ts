4dce2dceea4fbf3041c2d470c286c2a98e47d999
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-05-10 10:02:22 -0400

    started

---
+++ Assignment4/index.ts
@@ -0,0 +1,40 @@
+/**
+ * This function finds the palindrome depth of all
+ * teo digit numbers.
+ *
+ * By: Flynn Rundquist
+ * Version: 1.0
+ * Since: 2024-05-10
+ */
+
+
+// finds palindrome depth using recursion
+function findDepth(userInput: number, depth: number = 0) {
+    if (isPalindrome(userInput)) {
+        return depth
+    } else {
+        return findDepth(userInput + reverseNumber(userInput), depth + 1)
+    }
+}
+
+// checks if a number is a palindrome
+function isPalindrome(numberTest: number) {
+    return numberTest == reverseNumber(numberTest)
+}
+
+// reverses the number
+function reverseNumber(numberTest: number) {
+    let reversedNumber = 0
+    while (numberTest > 0) {
+        reversedNumber = reversedNumber * 10 + numberTest % 10
+        numberTest = Math.floor(numberTest / 10)
+    }
+    return reversedNumber
+}
+
+// find depth of all 2 digit numbers
+for (let counter = 10; counter < 100; counter++) {
+    console.log(`\nThe depth of ${counter} is ${findDepth(counter)}`)
+}
+
+console.log("\nDone")
